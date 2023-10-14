b4fb00ca1bf0da2c09e29737e47d0549f69fe5a2
Tran, Phuoc (Jon) <60534054+ttnnphuoc@users.noreply.github.com>
2023-10-14 17:00:15 +0700

    Open/Close Principle

---
+++ SOLIDExample/Salaries/BasicSalary.cs
@@ -0,0 +1,32 @@
+﻿namespace SOLIDExample.Salaries
+{
+    /// <summary>
+    /// Single Responsibility
+    /// Good :- A base abstract class with abstract method and children class override it according their needs
+    /// </summary>
+    public abstract class BasicSalary
+    {
+        public abstract double GetBasicSalary(EmployeeType employeeType);
+    }
+
+    /// <summary>
+    /// Open/Close Principle
+    /// </summary>
+    public class ParttimeEmployeeBasicSalary : BasicSalary
+    {
+        public override double GetBasicSalary(EmployeeType employeeType)
+        {
+            return 0;
+        }
+    }
+    /// <summary>
+    /// Open/Close Principle
+    /// </summary>
+    public class ContractEmployeeBasicSalary : BasicSalary
+    {
+        public override double GetBasicSalary(EmployeeType employeeType)
+        {
+            return 0;
+        }
+    }
+}
---
+++ SOLIDExample/Salaries/BasicSalaryCalulator.cs
@@ -0,0 +1,43 @@
+﻿using SOLIDExample.Entites;
+
+namespace SOLIDExample.Salaries
+{
+    /// <summary>
+    /// Original requirement
+    /// Example 
+    /// Bad :- A single method of class responsible for calculating basic salary , when new requirement comes we add a new if condition and solve it
+    /// </summary>
+    public class BasicSalaryCalulator
+    {
+        public decimal GetBasicSalary(EmployeeType employeeType)
+        {
+            decimal salary = 0;
+            if (employeeType == EmployeeType.Contract) { }
+            return salary;
+        }
+    }
+
+
+    /// <summary>
+    /// New requirement
+    /// </summary>
+    public class BasicSalaryCalulatorNew
+    {
+        public decimal GetBasicSalary(EmployeeType employeeType)
+        {
+            decimal salary = 0;
+            
+            if (employeeType == EmployeeType.Contract) { }
+            else  if (employeeType == EmployeeType.Parttime) { }
+
+            return salary;
+        }
+    }
+
+
+    public enum  EmployeeType
+    {
+        Contract,
+        Parttime
+    }
+}
