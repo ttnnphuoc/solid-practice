eead01d01afbca8def4463bc9158dd4fa3e50d31
Tran, Phuoc (Jon) <60534054+ttnnphuoc@users.noreply.github.com>
2023-10-15 16:10:31 +0700

    𝐃𝐨𝐧’𝐭 𝐑𝐞𝐩𝐞𝐚𝐭 𝐘𝐨𝐮𝐫𝐬𝐞𝐥𝐟 (𝐃𝐑𝐘) 𝐏𝐫𝐢𝐧𝐜𝐢𝐩𝐥𝐞

---
+++ SOLIDExample/DRY/DryClass.cs
@@ -0,0 +1,40 @@
+﻿namespace SOLIDExample.DRY
+{
+    #region BAD
+    public class Car1
+    {
+        public int Year { set; get; }
+        public string Model { set; get; } = string.Empty;
+        public string Make { set; get; } = string.Empty;
+        public int Type { set; get; }
+    }
+
+    public class Truck1
+    {
+        public int Year { set; get; }
+        public string Model { set; get; } = string.Empty;
+        public string Make { set; get; } = string.Empty;
+        public int Type { set; get; }
+    }
+    #endregion
+
+    #region GOOD
+    public class Vehicle
+    {
+        public int Year { set; get; }
+        public string Model { set; get; } = string.Empty;
+        public string Make { set; get; } = string.Empty;
+        public int Type { set; get; }
+    }
+    public class Car : Vehicle
+    {
+        public int NumDoor { set; get; }
+        public string EngineType { set; get; } = string.Empty;
+    }
+    public class Truck: Vehicle
+    {
+        public bool HasForWheelDrive { set; get; }
+        public double PayloadCapacity { set; get;}
+    }
+    #endregion
+}
